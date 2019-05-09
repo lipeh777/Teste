10562dac0ad0a2526408b4cc9ef4225b8246d6f8
Filipe Izidorio <31548630+lipeh777@users.noreply.github.com>
2019-05-09 11:38:21 -0300

    colocando os includes

---
+++ includes/mensagem.php
@@ -0,0 +1,23 @@
+<?php
+//sessao
+
+include_once 'session.php';
+
+
+	if(isset($_SESSION['mensagem'])): 
+?>
+		
+
+	<script>
+		//mensagem que mostra quando foi cadastrado com sucesso ou nao. MATERIALISE
+		window.onload = function (){
+			M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
+		};
+
+	</script>
+
+	
+<?php		
+	endif;
+	
+?>
